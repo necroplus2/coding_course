c043d5a669ad6fbded7bab46c8961e479eb48df1
sanrizk <necroplus2@gmail.com>
2023-01-20 14:28:07 +0700

    navbar edited

--- resources/views/course/layout/materi/sidebar.blade.php
+++ resources/views/course/layout/materi/sidebar.blade.php
@@ -38,50 +38,6 @@
                                 </div>
                             @endforeach
                         </div>
-                        <div class="sidebar">
-                            <div class="sidebar-section">
-                                <h5><span>Modul Pembahasan</span></h5>
-                                {{-- modul pembahasan atau di column tb db adalah bab --}}
-                                <ul style="list-none;">
-                                    @foreach ($bab as $b)
-                                        <li><a target="_blank" href="https://m.do.co/c/84c9b45d0c47"
-                                                class="read {{ $b->bab == $current_bab ? 'current' : '' }}">{{ $b->bab . '. ' . $b->judul }}</a>
-                                        </li>
-                                    @endforeach
-                                </ul>
-                            </div>
-
-                            <div class="sidebar-section">
-                                <h5><span>Bab</span></h5>
-                                {{-- bab atau di column tb db adalah sub_bab --}}
-                                <ul style="list-none;">
-                                    @foreach ($sub_bab as $sb)
-                                        <li><a target="_blank" href="https://m.do.co/c/84c9b45d0c47"
-                                                class="read {{ $sb->sub_bab == $current_sub_bab ? 'current' : '' }}">{{ $sb->sub_bab . '. ' . $sb->judul }}</a>
-                                        </li>
-                                    @endforeach
-                                    {{-- <li><a target="_blank" href="https://m.do.co/c/84c9b45d0c47" class="read">Digital Ocean</a></li>
-                                    <li><a target="blank"
-                                            href="https://www.cloudways.com/en/pricing.php?id=153986&amp;a_bid=005da123" class="read current">Cloudways</a>
-                                    </li>
-                                    <li><a target="blank"
-                                            href="http://shareasale.com/r.cfm?b=875645&amp;u=1087935&amp;m=41388&amp;urllink=&amp;afftrack=">Page
-                                            Speed Tesidebar-sectionst</a></li>
-                                    <li><a target="blank" href="https://elementor.com/?ref=1556">Elementor Page
-                                            Builder</a>
-                                    </li>
-                                    <li><a target="blank" href="https://www.wowthemes.net/category/jekyll-themes/">Our
-                                            Jekyll Themes</a></li> --}}
-                                </ul>
-                            </div>
-                            <div class="sidebar-section">
-                                <h5><span>Recommended</span></h5>
-                                <a href="https://easydigitaldownloads.com/?ref=2135"
-                                    title="Sell digital downloads with WordPress, for free"><img
-                                        src="https://easydigitaldownloads.com/wp-content/uploads/2015/02/300x250-2.png"
-                                        alt="Sell digital downloads with WordPress, for free"></a>
-                            </div>
-                        </div>
                     </div>
                 </div>
                 <!-- End Sidebar -->
